d0b4a506a282db36d11715927847e71f4dbe0b58
NdegeFelix <findech6@gmail.com>
2023-12-05 22:46:00 +0300

    completed assignment

--- lib/count_sentences.py
+++ lib/count_sentences.py
@@ -1,4 +1,27 @@
-#!/usr/bin/env python3
-
 class MyString:
-  pass
+    def __init__(self, value="Hello."):
+        if not isinstance(value, str):
+            raise ValueError("Value must be a string.")
+        self.value = value
+
+    def is_sentence(self):
+        return self.value.endswith('.')
+
+    def is_question(self):
+        return self.value.endswith('?')
+
+    def is_exclamation(self):
+        return self.value.endswith('!')
+
+    def count_sentences(self):
+        sentences = [sentence.strip() for sentence in self.value.split('.') if sentence.strip()]
+        sentences += [sentence.strip() for sentence in self.value.split('?') if sentence.strip()]
+        sentences += [sentence.strip() for sentence in self.value.split('!') if sentence.strip()]
+        return len(sentences)
+
+# Example usage:
+my_string = MyString("Hello.")
+print(my_string.is_sentence())  # Output: False
+print(my_string.is_question())  # Output: False
+print(my_string.is_exclamation())  # Output: False
+print(my_string.count_sentences())  # Output: 0
